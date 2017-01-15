0f79056dc9017e375fb0e47dcc19a3873fe85588
Trey Fortmuller <tfortmuller@mac.com>
2017-01-15 03:05:21 -0800

    initial commit with arduino sketches.

---
+++ bleuart_cmdmode/bleuart_cmdmode.ino
@@ -0,0 +1,316 @@
+#include <Arduino.h>
+#include <SPI.h>
+#if not defined (_VARIANT_ARDUINO_DUE_X_) && not defined (_VARIANT_ARDUINO_ZERO_)
+  #include <SoftwareSerial.h>
+#endif
+
+#include "Adafruit_BLE.h"
+#include "Adafruit_BluefruitLE_SPI.h"
+#include "Adafruit_BluefruitLE_UART.h"
+
+#include "BluefruitConfig.h"
+
+/*=========================================================================
+    APPLICATION SETTINGS
+
+    FACTORYRESET_ENABLE       Perform a factory reset when running this sketch
+   
+                              Enabling this will put your Bluefruit LE module
+                              in a 'known good' state and clear any config
+                              data set in previous sketches or projects, so
+                              running this at least once is a good idea.
+   
+                              When deploying your project, however, you will
+                              want to disable factory reset by setting this
+                              value to 0.  If you are making changes to your
+                              Bluefruit LE device via AT commands, and those
+                              changes aren't persisting across resets, this
+                              is the reason why.  Factory reset will erase
+                              the non-volatile memory where config data is
+                              stored, setting it back to factory default
+                              values.
+       
+                              Some sketches that require you to bond to a
+                              central device (HID mouse, keyboard, etc.)
+                              won't work at all with this feature enabled
+                              since the factory reset will clear all of the
+                              bonding data stored on the chip, meaning the
+                              central device won't be able to reconnect.
+    MINIMUM_FIRMWARE_VERSION  Minimum firmware version to have some new features
+    MODE_LED_BEHAVIOUR        LED activity, valid options are
+                              "DISABLE" or "MODE" or "BLEUART" or
+                              "HWUART"  or "SPI"  or "MANUAL"
+    -----------------------------------------------------------------------*/
+    #define FACTORYRESET_ENABLE         1
+    #define MINIMUM_FIRMWARE_VERSION    "0.6.6"
+    #define MODE_LED_BEHAVIOUR          "MODE"
+/*=========================================================================*/
+
+// Create the bluefruit object, either software serial...uncomment these lines
+/*
+SoftwareSerial bluefruitSS = SoftwareSerial(BLUEFRUIT_SWUART_TXD_PIN, BLUEFRUIT_SWUART_RXD_PIN);
+
+Adafruit_BluefruitLE_UART ble(bluefruitSS, BLUEFRUIT_UART_MODE_PIN,
+                      BLUEFRUIT_UART_CTS_PIN, BLUEFRUIT_UART_RTS_PIN);
+*/
+
+/* ...or hardware serial, which does not need the RTS/CTS pins. Uncomment this line */
+// Adafruit_BluefruitLE_UART ble(Serial1, BLUEFRUIT_UART_MODE_PIN);
+
+/* ...hardware SPI, using SCK/MOSI/MISO hardware SPI pins and then user selected CS/IRQ/RST */
+Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
+
+/* ...software SPI, using SCK/MOSI/MISO user-defined SPI pins and then user selected CS/IRQ/RST */
+//Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_SCK, BLUEFRUIT_SPI_MISO,
+//                             BLUEFRUIT_SPI_MOSI, BLUEFRUIT_SPI_CS,
+//                             BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
+
+
+// A small helper
+void error(const __FlashStringHelper*err) {
+  Serial.println(err);
+  while (1);
+}
+
+/**************************************************************************/
+/*!
+    @brief  Sets up the HW an the BLE module (this function is called
+            automatically on startup)
+*/
+/**************************************************************************/
+
+
+#include <AccelStepper.h>
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_MS_PWMServoDriver.h"
+
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+
+// Connect two steppers with 200 steps per revolution (1.8 degree)
+// to the top shield
+Adafruit_StepperMotor *myStepper1 = AFMS.getStepper(200, 1);
+Adafruit_StepperMotor *myStepper2 = AFMS.getStepper(200, 2);
+// you can change these to DOUBLE or INTERLEAVE or MICROSTEP!
+// wrappers for the first motor!
+void forwardstep1() {  
+  myStepper1->onestep(BACKWARD, SINGLE);
+}
+void backwardstep1() {  
+  myStepper1->onestep(FORWARD, SINGLE);
+}
+// wrappers for the second motor!
+void forwardstep2() {  
+  myStepper2->onestep(FORWARD, SINGLE);
+}
+void backwardstep2() {  
+  myStepper2->onestep(BACKWARD, SINGLE);
+}
+
+// Now we'll wrap the 3 steppers in an AccelStepper object
+AccelStepper stepper1(forwardstep1, backwardstep1);
+AccelStepper stepper2(forwardstep2, backwardstep2);
+
+
+void setup(void)
+{
+  while (!Serial);  // required for Flora & Micro
+  delay(500);
+  pinMode(10, OUTPUT);
+
+  Serial.begin(115200);
+  Serial.println(F("Adafruit Bluefruit Command Mode Example"));
+  Serial.println(F("---------------------------------------"));
+
+  /* Initialise the module */
+  Serial.print(F("Initialising the Bluefruit LE module: "));
+
+  if ( !ble.begin(VERBOSE_MODE) )
+  {
+    error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
+  }
+  Serial.println( F("OK!") );
+
+  if ( FACTORYRESET_ENABLE )
+  {
+    /* Perform a factory reset to make sure everything is in a known state */
+    Serial.println(F("Performing a factory reset: "));
+    if ( ! ble.factoryReset() ){
+      error(F("Couldn't factory reset"));
+    }
+  }
+
+  /* Disable command echo from Bluefruit */
+  ble.echo(false);
+
+  Serial.println("Requesting Bluefruit info:");
+  /* Print Bluefruit information */
+  ble.info();
+
+  Serial.println(F("Please use Adafruit Bluefruit LE app to connect in UART mode"));
+  Serial.println(F("Then Enter characters to send to Bluefruit"));
+  Serial.println();
+
+  ble.verbose(false);  // debug info is a little annoying after this point!
+
+  /* Wait for connection */
+  while (! ble.isConnected()) {
+      delay(500);
+  }
+
+  // LED Activity command is only supported from 0.6.6
+  if ( ble.isVersionAtLeast(MINIMUM_FIRMWARE_VERSION) )
+  {
+    // Change Mode LED Activity
+    Serial.println(F("******************************"));
+    Serial.println(F("Change LED activity to " MODE_LED_BEHAVIOUR));
+    ble.sendCommandCheckOK("AT+HWModeLED=" MODE_LED_BEHAVIOUR);
+    Serial.println(F("******************************"));
+  }
+
+  AFMS.begin(); // Start the top shield
+
+  // set speed and accel parameters for the stepper motors
+  stepper1.setMaxSpeed(50.0);
+  stepper1.setAcceleration(400.0);
+    
+  stepper2.setMaxSpeed(50.0);
+  stepper2.setAcceleration(400.0);
+  
+}
+
+void forward() {
+  stepper1.moveTo(stepper1.currentPosition() - 100);
+  stepper2.moveTo(stepper2.currentPosition() - 100);
+  stepper1.run();
+  stepper2.run();
+  stepper2.moveTo(stepper1.currentPosition() - 100);
+  stepper1.moveTo(stepper2.currentPosition() - 100);
+  stepper2.run();
+  stepper1.run(); 
+}
+
+void right() {
+  stepper1.moveTo(stepper1.currentPosition() + 300);
+  stepper2.moveTo(stepper2.currentPosition() - 300);
+  stepper1.run();
+  stepper2.run(); 
+}
+
+void left() {
+  stepper1.moveTo(stepper1.currentPosition() - 300);
+  stepper2.moveTo(stepper2.currentPosition() + 300);
+  stepper1.run();
+  stepper2.run(); 
+}
+
+void backward() {
+  stepper1.moveTo(stepper1.currentPosition() + 100);
+  stepper2.moveTo(stepper2.currentPosition() + 100);
+  stepper1.run();
+  stepper2.run(); 
+}
+
+/**************************************************************************/
+/*!
+    @brief  Constantly poll for new command or response data
+*/
+/**************************************************************************/
+String previousCommand = "stop";
+
+void loop(void)
+{
+  // Check for user input
+  char inputs[BUFSIZE+1];
+
+  if ( getUserInput(inputs, BUFSIZE) )
+  {
+    // Send characters to Bluefruit
+    Serial.print("[Send] ");
+    Serial.println(inputs);
+
+    ble.print("AT+BLEUARTTX=");
+    ble.println(inputs);
+
+    // check response stastus
+    if (! ble.waitForOK() ) {
+      Serial.println(F("Failed to send?"));
+    }
+  }
+
+  String str;
+  str = previousCommand;
+  if (ble.buffer) {  
+      str = ble.buffer;
+      previousCommand = str;
+  }
+
+  while (str != "stop") {
+    
+      Serial.println(str);
+     
+      if (str == "f") {
+          forward();
+      } else if (str == "r") {
+        right();
+      } else if (str == "l") {
+        left();  
+      } else if (str == "b") {
+        backward();
+      } else if (str == "stop") {
+        stepper1.stop();
+        stepper2.stop();
+      }
+
+        // Check for incoming characters from Bluefruit
+        ble.println("AT+BLEUARTRX");
+        ble.readline();
+
+        // Some data was found, its in the buffer
+        Serial.println(F("[Recv] ")); 
+      //  Serial.write(ble.buffer);
+       
+      if (ble.buffer == "stop") {  
+        str = ble.buffer;
+        previousCommand = "stop";
+
+      } else if (strcmp(ble.buffer, "OK") == 0) {
+        str = previousCommand;
+      } else {
+        previousCommand = ble.buffer;
+      }
+  }
+  
+    
+
+  digitalWrite(10, HIGH);
+  delay(500);
+  digitalWrite(10, LOW);
+  ble.waitForOK();
+}
+
+/**************************************************************************/
+/*!
+    @brief  Checks for user input (via the Serial Monitor)
+*/
+/**************************************************************************/
+bool getUserInput(char buffer[], uint8_t maxSize)
+{
+  // timeout in 100 milliseconds
+  TimeoutTimer timeout(100);
+
+  memset(buffer, 0, maxSize);
+  while( (!Serial.available()) && !timeout.expired() ) { delay(1); }
+
+  if ( timeout.expired() ) return false;
+
+  delay(2);
+  uint8_t count=0;
+  do
+  {
+    count += Serial.readBytes(buffer+count, maxSize);
+    delay(2);
+  } while( (count < maxSize) && (Serial.available()) );
+
+  return true;
+}
